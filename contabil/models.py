e9b29ddde24aa09edd5d9f8635d8606e46b0a132
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-25 16:22:25 -0300

    feat: created PurchaseInvocieItems model

---
+++ contabil/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime as dt
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class PurchaseInvoiceItems(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    invoice_number = models.IntegerField(verbose_name=_('Número da nota fiscal'))
+    supplier = models.ForeignKey('supply.Fornecedor', on_delete=models.CASCADE, verbose_name=_('Fornecedor'))
+    issue_date = models.DateField(default=dt.now, verbose_name=_('Data de emissão'))
+    product_name = models.CharField(max_length=255, verbose_name=_('Nome do produto'))
+    product_category = models.CharField(max_length=255, verbose_name=_('Categoria do produto'))
+    quantity = models.DecimalField(default=0.0, decimal_places=2, max_digits=10, verbose_name=_('Quantidade'))
+    total_amount = models.DecimalField(default=0.0, decimal_places=2, max_digits=10, verbose_name=_('Valor total'))
+
+    class Meta:
+        verbose_name = _('Item da nota fiscal de compra')
+        verbose_name_plural = _('Itens da nota fiscal de compra')
+
+    def __str__(self):
+        return f"{self.invoice_number} - {self.product_name}"
