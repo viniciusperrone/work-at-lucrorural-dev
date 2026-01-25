a16c51f51552155456eea95308c213e0980c2ef6
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-24 23:16:56 -0300

    refactor: improved Fornecedor model

--- supply/models.py
+++ supply/models.py
@@ -6,5 +6,12 @@ from uuid import uuid4
 class Fornecedor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome = models.CharField(max_length=100)
-    cnpj = models.CharField(max_length=100)
-    telefone = models.CharField(min_length= 10, max_length=11)
+    cnpj = models.CharField(max_length=18)
+    telefone = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Fornecedor'
+        verbose_name_plural = 'Fornecedores'
+
+    def __str__(self):
+        return self.nome
