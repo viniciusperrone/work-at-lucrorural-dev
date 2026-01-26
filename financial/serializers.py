401f58498d1a6177551d8bef9f65412ca01b3c7c
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-26 16:05:57 -0300

    fix: improving AccountPayableSerializer

--- financial/serializers.py
+++ financial/serializers.py
@@ -8,3 +8,25 @@ class AccountPayableSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountPayable
         fields = '__all__'
+        read_only_fields = ('id', 'total_amount')
+
+    def validate(self, data):
+        supplier = data.get('supplier') or self.instance.supplier
+        invoices = data.get('invoices')
+
+        if not invoices:
+            return data
+
+        if invoices:
+            invalid = [
+                invoice.id
+                for invoice in invoices
+                if invoice.supplier_id != supplier.id
+            ]
+
+            if len(invalid) > 0:
+                raise serializers.ValidationError({
+                    'invoices': 'Todas as notas fiscais devem pertencer ao mesmo fornecedor'
+                })
+
+        return data
