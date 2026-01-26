9bdb5aaee32dee9798ae28305b2fb014352067bc
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-26 18:16:19 -0300

    fix: fixed default value for issue_date attribute

--- contabil/models.py
+++ contabil/models.py
@@ -1,15 +1,15 @@
-from datetime import datetime as dt
 import uuid
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 
 class Invoice(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     invoice_number = models.IntegerField(verbose_name=_('Número da nota fiscal'))
     supplier = models.ForeignKey('supply.Supplier', on_delete=models.CASCADE, verbose_name=_('Fornecedor'))
-    issue_date = models.DateField(default=dt.now, verbose_name=_('Data de emissão'))
+    issue_date = models.DateField(default=timezone.now, verbose_name=_('Data de emissão'))
     product_name = models.CharField(max_length=255, verbose_name=_('Nome do produto'))
     product_category = models.CharField(max_length=255, verbose_name=_('Categoria do produto'))
     quantity = models.DecimalField(default=0.0, decimal_places=2, max_digits=10, verbose_name=_('Quantidade'))
