c66ad065dd623440c7025f5de2bab9522fa3d8b2
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-27 02:01:11 -0300

    test: created test_encryption_phone

---
+++ supply/tests/test_encryption.py
@@ -0,0 +1,19 @@
+"""
+    Here are the unit tests for encryption and
+    descryption of phone from registered suppliers.
+"""
+
+from django.test import TestCase
+
+from supply.models import Supplier
+
+
+class SupplierEncryptionTestCase(TestCase):
+
+    def test_encryption_phone(self):
+        phone = "(41) 99999-9999"
+        encrypted = Supplier.encrypt_phone(phone)
+
+        self.assertNotEquals(encrypted, phone)
+        self.assertTrue(encrypted.startswith("gAAAA"))
+
